042848d6933bcdc5292a127e1db0811984742485
dulces suenos <146747623+procodeshiter@users.noreply.github.com>
2024-06-10 18:49:31 +0300

    task.js

--- task.js
+++ task.js
@@ -35,7 +35,7 @@ function markButtonAsCompleted(button) {
 }
 
 function addToBalanceAndDisplay(amount) {
-    if (amount === 1 || amount === 2 ) {
+    if (amount === 5 || amount === 6 ) {
         return; // Не добавляем ничего к балансу для третьей и четвёртой кнопок
     }
 
